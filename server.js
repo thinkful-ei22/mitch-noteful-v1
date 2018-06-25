f670927bb9b95d77d58db3c123608d3f3b830a61
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-25 15:04:11 -0400

    endpoint set up for listing all notes

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params;
+  res.json(data[id]);
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
