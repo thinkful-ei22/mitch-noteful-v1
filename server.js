8639ac9a3ed79f889b092a7b18636774bde9683c
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-25 14:57:09 -0400

    created express app that can serve static files

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 'use strict';
 
+// Load express
+const express = require('express');
 // Load array of notes
 const data = require('./db/notes');
+// Initialize express app
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
 
 console.log('Hello Noteful!');
 
