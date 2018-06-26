26bf243e1a477dc532f402513491731ae734c9a4
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-26 14:36:06 -0400

    added express error-handling middleware

--- server.js
+++ server.js
@@ -19,9 +19,27 @@ app.get('/api/notes', (req, res) => {
   res.json(searchTerm ? data.filter(item => item.title.includes(searchTerm)) : data);
 });
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id/', (req, res) => {
   const id = req.params.id;
   res.json(data.find(note => note.id === Number(id)));
+});
+
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 app.listen(PORT, function () {
