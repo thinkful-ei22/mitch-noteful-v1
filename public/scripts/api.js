030785522e9d2b9a988ac015a2cfd949d5650dd2
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-26 15:34:30 -0400

    client updated

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -20,5 +20,16 @@ const api = {
       url: `/api/notes/${id}`,
       success: callback
     });
+  },
+
+  update: function(id, obj, callback) {
+    $.ajax({
+      type: 'PUT',
+      url: `/api/notes/${id}`,
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify(obj),
+      success: callback
+    });
   }
 };
